e995c4bb023bedafefde4f0d7586cfafb2a4fae9
Rhodinium <103146200+Karxii@users.noreply.github.com>
2022-06-23 00:18:21 +0100

    Encrypted push 1

---
+++ resources/[encrypted]/[casino]/casino-walls/fxmanifest.lua
@@ -0,0 +1,25 @@
+fx_version 'cerulean'
+games      { 'gta5' }
+
+lua54 'yes'
+
+escrow_ignore {
+ 'config.lua'
+}
+
+shared_scripts {
+	'config.lua',
+}
+
+
+client_scripts{
+	'@PolyZone/client.lua',
+    	'@PolyZone/CircleZone.lua',
+	'client/*.lua'
+} 
+
+server_scripts{
+	'server/*.lua'
+} 
+
+dependency '/assetpacks'
---
+++ resources/[encrypted]/[qb]/qb-backitems/fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+lua54 'yes'
+
+escrow_ignore {
+	'config.lua'
+}
+
+client_scripts {
+    'client.lua',
+    'BackItems.lua',
+}
+
+dependency '/assetpacks'
